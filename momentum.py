83beb47e07f69df7c7659fa1b1dc00f63dec365b
Harsh Agrawal <110854466+harshag2315@users.noreply.github.com>
2023-01-26 00:14:11 +0530

    Add files via upload
    Some problem solving codes:

---
+++ momentum.py
@@ -0,0 +1,4 @@
+mass=float(input('Enter the mass of the object(in kilograms):'))
+vel=float(input('Enter the velocity of the object(in meters per seconds):'))
+energy=mass*(vel**2)
+print(f'The energy:{energy}')
---
+++ second_cal.py
@@ -0,0 +1,6 @@
+#write a program to calculate number of seconds in a day
+hour=24
+min=60
+sec=60
+day=int(input('Enter number of days:'))
+print(f'The number of seconds in {day} day is {day*hour*min*sec}')
